d833c0e0d618b4ea3c55fa2fe536ed47f1e4320b
David Wiesenberg <david_britta@yahoo.com>
2016-12-15 13:35:05 -0500

    reload gem_building_practice files

---
+++ lib/wiesenberg_gem_building_practice.rb
@@ -0,0 +1,5 @@
+require "wiesenberg_gem_building_practice/version"
+
+module WiesenbergGemBuildingPractice
+  # Your code goes here...
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,2 @@
+$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
+require "wiesenberg_gem_building_practice"
